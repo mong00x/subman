6ccb2f2a4d6a88a35c1e46782ef41337078525b3
Meng Yuan <68982813+mong00x@users.noreply.github.com>
2021-07-14 16:12:58 +0930

    T162 react home page total subs (#13)
    * created home page and components on it. style and logic of nav. added 2 npm packages
    
    * created total subs component. deleted react-icons package

--- frontend/src/components/TotalSubs.jsx
+++ frontend/src/components/TotalSubs.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import {useState} from 'react'
+import {ReactComponent as DownButton} from '../icons/chevron-down.svg'
 
 const TotalSubs = () => {
+    const [showDetail, setShowDetail] = useState(false)
+    const onClick = () =>{
+        setShowDetail(!showDetail)
+        console.log(showDetail)
+    }
     return (
-        <div>
-            
+        <div className={showDetail? "total-subs expand" : "total-subs"}>
+            <div className="total flex space-between">
+                <div className="textarea">
+                    <p className="f3">Total for you subscriptions:</p>
+                    <span className="f5 bold">$156.9</span><span className="f2 gray"> /per month</span>
+                </div>
+                <DownButton className={showDetail? "rotate down" : "rotate" }onClick={onClick}/>
+            </div>
+        {showDetail ? 
+            (<div className="">
+                <hr/>
+                <ul className="detail textarea f2">
+                    <li className="flex space-between">
+                        <span>Entertainment:</span><span>$52.6</span>
+                    </li>
+                    <li className="flex space-between">
+                        <span>Utilities:</span><span>$52.6</span>
+                    </li>
+                    <li className="flex space-between">
+                        <span>Work:</span><span>$52.6</span>
+                    </li>
+                    <li className="flex space-between">
+                        <span>Other:</span><span>$52.6</span>
+                    </li>
+                </ul>
+            </div>):(null)
+        }
+        
         </div>
     )
 }
--- frontend/src/views/HomePage.jsx
+++ frontend/src/views/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Nav from '../components/Nav'
+import TotalSubs from '../components/TotalSubs'
 
 const HomePage = () => {
     return (
         <div className="container">
             Home Page
             <Nav/>
+            <TotalSubs />
         </div>
     )
 }
