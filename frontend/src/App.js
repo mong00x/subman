4816aab95049e9378b0e404d9b644130b053a4c4
mitchelljphayes <18720183+mitchelljphayes@users.noreply.github.com>
2021-07-15 18:30:02 +1000

    Create subs (#25)
    * rebased on main
    
    * attempting to add auth
    
    * installed dj-rest-auth
    
    * installed django-eventtools
    
    * removed category
    
    * added logo path
    
    * migration 007 -> licence to migrate
    
    * moded categories into subscriptions added logo path
    
    * connected data to homepage
    
    * merge main
    
    * rebuild package-lock.json
    
    * moved logic into homepage
    
    * homepage tweaks
    
    * change api fetch to async

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,7 @@
-import axios from "axios";
 // import "./App.css";
 import HomePage from "./views/HomePage.jsx";
 import AddPage from "./views/AddPage.jsx";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
-// const axios = require('axios').default;
-
-const subsList = axios.get("/subscription/")
-  .then((response) => {console.log(response)})
-  .catch((error) => {console.log(error)})
-  .then(() => {console.log('logging get request')})
-
-console.log(subsList);
-
 
 function App() {
 
--- frontend/src/views/HomePage.jsx
+++ frontend/src/views/HomePage.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Nav from '../components/Nav'
 import TotalSubs from '../components/TotalSubs'
 import Sub from "../components/Sub"
 import AddButton from "../components/AddButton"
 import RecentSubs from '../components/RecentSubs'
 import Chart from '../components/Chart'
+import axios from "axios";
 
 /* TEMPORARY, FOR TESTING PURPOSES */
 import {ReactComponent as YoutubeLogo} from '../icons/youtube-logo.svg'
@@ -12,59 +13,95 @@ import {ReactComponent as PieChart} from '../icons/pie-chart.svg'
 import {ReactComponent as PlusIcon} from '../icons/plus.svg'
 
 
-
-const HomePage = (props) => {
-
-    return (
-        <div className="container">
-            Home Page
-            <Nav />
-            <TotalSubs />
-            <div className=" flex row overflow-x">
-                <AddButton buttonIcon = {<PlusIcon className="button-icon"/>}
-                buttonMessage = "Add Subscription"/>
-                <RecentSubs />
-                <RecentSubs />
-                <RecentSubs />
-            </div>
-                
-            <div style={{margin:"10px 0"}}>
-                <b>Up comming payments(days)</b>
-
-                <Sub 
-                    subIcon={<YoutubeLogo className="sub-icon"/>}
-                    subName="Youtube Premium"
-                    subPrice="$99.99"
-                    subTimePeriod="month"
-                    subTimeLeftIcon={<Chart className=""/>}
-                />
-
-
-                {/* <Sub 
-                    subIcon={<YoutubeLogo className="sub-icon"/>}
-                    subName="Netflix"
-                    subPrice="$32.50"
-                    subTimePeriod="week"
-                    subTimeLeftIcon={<PieChart className="sub-time-left-icon"/>}
-                />
-                <Sub 
-                    subIcon={<YoutubeLogo className="sub-icon"/>}
-                    subName="Figma"
-                    subPrice="$100"
-                    subTimePeriod="year"
-                    subTimeLeftIcon={<PieChart className="sub-time-left-icon"/>}
-                />
-                <Sub 
-                    subIcon={<YoutubeLogo className="sub-icon"/>}
-                    subName="Monash University"
-                    subPrice="$10000"
-                    subTimePeriod="year"
-                    subTimeLeftIcon={<PieChart className="sub-time-left-icon"/>}
-                /> */}
-            </div>
+const HomePage = () => {
+    
+    
+        function SubscriptionCardList() {
             
-        </div>
-    )
-}
+            const [subsData, setSubscriptionData] = useState({});
+    
+            useEffect(() => {
+                getSubscriptions();
+            }, []);
+            
+            const getSubscriptions = async () => {
+                const response = await axios.get("/subscription/");
+                console.log(response.data, 'getSubscriptions')
+                setSubscriptionData(response);
 
-export default HomePage
+            };
+            
+            
+            return (
+                <div>
+                    {subsData.content.data.map(object => {
+                        if (typeof object !== "undefined") {
+                            
+                                <Sub 
+                                    subIcon={object.subscription_logo_path}
+                                    subName={object.subscription_name}
+                                    subPrice={object.subscription_cost}
+                                    subTimePeriod={object.subscription_frequency}
+                                    subTimeLeftIcon={<Chart className=""/>}
+                                /> 
+                             
+                        }
+                    })}
+                </div>
+            )
+        }
+    
+        return (
+            <div className="container">
+                Home Page
+                <Nav />
+                <TotalSubs />
+                <div className=" flex row overflow-x">
+                    <AddButton buttonIcon = {<PlusIcon className="button-icon"/>}
+                    buttonMessage = "Add Subscription"/>
+                    <RecentSubs />
+                    <RecentSubs />
+                    <RecentSubs />
+                </div>
+                    
+                <div style={{margin:"10px 0"}}>
+                    <b>Up comming payments(days)</b>
+                    
+                    <SubscriptionCardList />
+                    
+                    <Sub 
+                        subIcon={<YoutubeLogo className="sub-icon"/>}
+                        subName='{props.data.subscription_name}'
+                        subPrice="$99.99"
+                        subTimePeriod="month"
+                        subTimeLeftIcon={<Chart className=""/>}
+                    />
+                    <Sub 
+                        subIcon={<YoutubeLogo className="sub-icon"/>}
+                        subName="Netflix"
+                        subPrice="$32.50"
+                        subTimePeriod="week"
+                        subTimeLeftIcon={<PieChart className="sub-time-left-icon"/>}
+                    />
+                    <Sub 
+                        subIcon={<YoutubeLogo className="sub-icon"/>}
+                        subName="Figma"
+                        subPrice="$100"
+                        subTimePeriod="year"
+                        subTimeLeftIcon={<PieChart className="sub-time-left-icon"/>}
+                    />
+                    <Sub 
+                        subIcon={<YoutubeLogo className="sub-icon"/>}
+                        subName="Monash University"
+                        subPrice="$10000"
+                        subTimePeriod="year"
+                        subTimeLeftIcon={<PieChart className="sub-time-left-icon"/>}
+                    />
+                </div>
+                
+            </div>
+        )
+    }
+    
+    export default HomePage
+
