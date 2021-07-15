9bd653307b1128bf23fd0cb38bbe74b7e279e2b4
Meng Yuan <68982813+mong00x@users.noreply.github.com>
2021-07-15 19:16:04 +0930

    get data from backend (#26)

--- frontend/src/components/Sub.jsx
+++ frontend/src/components/Sub.jsx
@@ -3,23 +3,31 @@ import {ReactComponent as YoutubeLogo} from '../icons/youtube-logo.svg'
 import {ReactComponent as PieChart} from '../icons/pie-chart.svg'
 
 
-const Sub = (props) => {
+const Sub = ({name, price, period}) => {
+    console.log(name)
     return (
         <div className="sub-item flex space-between">
             <div className="sub-icon-background flex">
-                {props.subIcon}
+            
             </div>
             <div className="sub-description flex">
                 <div className="textarea">
-                    <p className="f3">{props.subName}</p>
-                    <span className="f5 bold">{props.subPrice}</span><span className="f2 gray"> /per {props.subTimePeriod}</span>
+                    <p className="f3">{name}</p>
+                    <span className="f5 bold">{price}</span><span className="f2 gray"> /per {period}</span>
                 </div>
                 
             </div>
             <div className="sub-time-left-icon-background flex">
-                {props.subTimeLeftIcon}
+                {}
             </div>
+            <div>
+            <p>
+            {name}
+
+            </p>
         </div>
+        </div>
+        
     )
 }
 
--- frontend/src/views/HomePage.jsx
+++ frontend/src/views/HomePage.jsx
@@ -1,11 +1,12 @@
-import React, { useEffect, useState } from 'react'
 import Nav from '../components/Nav'
 import TotalSubs from '../components/TotalSubs'
 import Sub from "../components/Sub"
 import AddButton from "../components/AddButton"
 import RecentSubs from '../components/RecentSubs'
 import Chart from '../components/Chart'
-import axios from "axios";
+
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 
 /* TEMPORARY, FOR TESTING PURPOSES */
 import {ReactComponent as YoutubeLogo} from '../icons/youtube-logo.svg'
@@ -13,95 +14,83 @@ import {ReactComponent as PieChart} from '../icons/pie-chart.svg'
 import {ReactComponent as PlusIcon} from '../icons/plus.svg'
 
 
-const HomePage = () => {
-    
-    
-        function SubscriptionCardList() {
-            
-            const [subsData, setSubscriptionData] = useState({});
-    
-            useEffect(() => {
-                getSubscriptions();
-            }, []);
-            
-            const getSubscriptions = async () => {
-                const response = await axios.get("/subscription/");
-                console.log(response.data, 'getSubscriptions')
-                setSubscriptionData(response);
 
-            };
-            
-            
-            return (
-                <div>
-                    {subsData.content.data.map(object => {
-                        if (typeof object !== "undefined") {
-                            
-                                <Sub 
-                                    subIcon={object.subscription_logo_path}
-                                    subName={object.subscription_name}
-                                    subPrice={object.subscription_cost}
-                                    subTimePeriod={object.subscription_frequency}
-                                    subTimeLeftIcon={<Chart className=""/>}
-                                /> 
-                             
-                        }
-                    })}
-                </div>
-            )
-        }
+const HomePage = (props) => {
+    const [subsDatas, setSubscriptionDatas] = useState([]);
+
+    useEffect(() => {
+        getSubscriptions();
+    }, []);
     
-        return (
-            <div className="container">
-                Home Page
-                <Nav />
-                <TotalSubs />
-                <div className=" flex row overflow-x">
-                    <AddButton buttonIcon = {<PlusIcon className="button-icon"/>}
-                    buttonMessage = "Add Subscription"/>
-                    <RecentSubs />
-                    <RecentSubs />
-                    <RecentSubs />
-                </div>
-                    
-                <div style={{margin:"10px 0"}}>
-                    <b>Up comming payments(days)</b>
-                    
-                    <SubscriptionCardList />
-                    
-                    <Sub 
-                        subIcon={<YoutubeLogo className="sub-icon"/>}
-                        subName='{props.data.subscription_name}'
-                        subPrice="$99.99"
-                        subTimePeriod="month"
-                        subTimeLeftIcon={<Chart className=""/>}
-                    />
-                    <Sub 
-                        subIcon={<YoutubeLogo className="sub-icon"/>}
-                        subName="Netflix"
-                        subPrice="$32.50"
-                        subTimePeriod="week"
-                        subTimeLeftIcon={<PieChart className="sub-time-left-icon"/>}
-                    />
-                    <Sub 
-                        subIcon={<YoutubeLogo className="sub-icon"/>}
-                        subName="Figma"
-                        subPrice="$100"
-                        subTimePeriod="year"
-                        subTimeLeftIcon={<PieChart className="sub-time-left-icon"/>}
-                    />
-                    <Sub 
-                        subIcon={<YoutubeLogo className="sub-icon"/>}
-                        subName="Monash University"
-                        subPrice="$10000"
-                        subTimePeriod="year"
-                        subTimeLeftIcon={<PieChart className="sub-time-left-icon"/>}
-                    />
-                </div>
-                
+    const getSubscriptions = async () => {
+        const response = await axios.get("/subscription/");
+        // console.log(response.data, 'getSubscriptions')
+        setSubscriptionDatas([...response.data])
+    };
+    console.log(...subsDatas)
+    // console.log(subsDatas)
+
+    
+    return (
+        <div className="container">
+            Home Page
+            <Nav />
+            <TotalSubs />
+            <div className=" flex row overflow-x">
+                <AddButton buttonIcon = {<PlusIcon className="button-icon"/>}
+                buttonMessage = "Add Subscription"/>
+                <RecentSubs />
+                <RecentSubs />
+                <RecentSubs />
             </div>
-        )
-    }
-    
-    export default HomePage
+                
+            <div style={{margin:"10px 0"}}>
+                <b>Up comming payments(days)</b>
+            {subsDatas.map((Data) => <Sub 
+                icon={Data.subscription_logo_path}
+                name={Data.subscription_name}
+                price={Data.subscription_cost} 
+                period={Data.subscription_frequency}
+            />)}
+                
 
+                
+
+            {/* <Sub 
+                    subIcon={<YoutubeLogo className="sub-icon"/>}
+                    subName="Youtube Premium"
+                    subPrice="$99.99"
+                    subTimePeriod="month"
+                    subTimeLeftIcon={<Chart className=""/>}
+                /> */}
+
+
+                {/* <Sub 
+                    subIcon={<YoutubeLogo className="sub-icon"/>}
+                    subName="Netflix"
+                    subPrice="$32.50"
+                    subTimePeriod="week"
+                    subTimeLeftIcon={<PieChart className="sub-time-left-icon"/>}
+                />
+                <Sub 
+                    subIcon={<YoutubeLogo className="sub-icon"/>}
+                    subName="Figma"
+                    subPrice="$100"
+                    subTimePeriod="year"
+                    subTimeLeftIcon={<PieChart className="sub-time-left-icon"/>}
+                />
+                <Sub 
+                    subIcon={<YoutubeLogo className="sub-icon"/>}
+                    subName="Monash University"
+                    subPrice="$10000"
+                    subTimePeriod="year"
+                    subTimeLeftIcon={<PieChart className="sub-time-left-icon"/>}
+                /> */}
+            </div>
+            
+        </div>
+    )
+
+}
+
+export default HomePage
